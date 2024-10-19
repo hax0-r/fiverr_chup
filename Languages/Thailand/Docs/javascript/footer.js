c4e46d53980963d892f27451f83a242be3c12b93
Muhammad <talha18513@gmail.com>
2024-10-19 15:34:25 +0500

    I added text to the file

--- Languages/Thailand/Docs/javascript/footer.js
+++ Languages/Thailand/Docs/javascript/footer.js
@@ -2,23 +2,23 @@ document.getElementById('dynamic-footer').innerHTML = `
     <div class="px-5 md:px-12 2xl:px-0 bg-[#115934] md:pt-16 pt-10 md:pb-5 pb-3">
         <div class="max-w-[80rem] mx-auto">
             <div class="flex md:gap-28 md:flex-row flex-col gap-5">
-                <a href="/Languages/Mandarin/index.html">
-                    <img src="/Docs/Assets/Images/Global/fullWhiteLogo.svg" class="max-w-[103.43px] w-full" alt="">
+                <a href="/Languages/Thailand/index.html">
+                    <img src="/Docs/Assets/Images/Global/thaiWhiteLogo.svg" class="max-w-[103.43px] w-full" alt="">
                 </a>
                 <div class="w-full">
                     <ul class="grid text-white md:grid-cols-4 grid-cols-3 gap-y-2 w-full md:text-[16px] text-[12px]">
-                        <a href="/Languages/Mandarin/about.html"><li>เกี่ยวกับเรา</li></a>
-                        <a href="/Languages/Mandarin/case-study.html"><li>案例研究</li></a>
-                        <a href="/Languages/Mandarin/News/chup-news.html"><li>新闻与博客</li></a>
-                        <a href="/Languages/Mandarin/Products.html"><li>产品</li></a>
-                        <a href="/Languages/Mandarin/FAQ.html"><li>常见问题</li></a>
-                        <a href="/Languages/Mandarin/Career.html"><li>职业生涯</li></a>
-                        <a href="/Languages/Mandarin/Security-Compliance.html"><li class="text-nowrap">安全合规</li></a>
+                        <a href="/Languages/Thailand/about.html"><li>เกี่ยวกับเรา</li></a>
+                        <a href="/Languages/Thailand/case-study.html"><li>กรณีศึกษา</li></a>
+                        <a href="/Languages/Thailand/News/chup-news.html"><li>ข่าวสารและบล็อก</li></a>
+                        <a href="/Languages/Thailand/Products.html"><li>ผลิตภัณฑ์</li></a>
+                        <a href="/Languages/Thailand/FAQ.html"><li>คำถามทั่วไป</li></a>
+                        <a href="/Languages/Thailand/Career.html"><li>ร่วมงานกับเรา</li></a>
+                        <a href="/Languages/Thailand/Security-Compliance.html"><li class="text-nowrap">การปฏิบัติตามข้อกำหนดด้านความปลอดภัย</li></a>
                     </ul>
                 </div>
             </div>
             <div class="w-full h-[1px] bg-white md:mt-10 mt-6 mb-5"></div>
-            <p class="text-white text-center">© Chup Online Sdn Bhd. 保留所有权利.</p>
+            <p class="text-white text-center">© 2024 jongQ Sdn Bhd. สงวนลิขสิทธิ์</p>
         </div>
     </div>
 `;
